6a524623d79bb2c572e3c4d85f0c96b007aba8ed
Niall Caplis <niallcaplis@gmail.com>
2025-03-06 14:14:16 +0000

    remove unnecessary assignment from top of board.js

--- board.js
+++ board.js
@@ -1,10 +1,8 @@
-const positions = document.startPositions;
-
-
 const Board = () => {
     const ranks = ["8", "7", "6", "5", "4", "3", "2", "1"];
     const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    return(
+
+    return (
         <table>
             <tbody>
                 {ranks.map((rank, rankIndex) => (
